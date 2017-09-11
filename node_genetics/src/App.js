76eb0f40a69f7f48e478e9a3aac2e5b0f5b750fb
Tracey Wright <traceywright27@gmail.com>
2017-09-11 19:14:02 +1000

    interim commit

--- node_genetics/src/App.js
+++ node_genetics/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-// function inputSequence() {
-//   let seqInput = document.getElementById('seqInput').value;
-//   console.log(seqInput)
-//   this.setState({ seq: seqInput })
-//   console.log(this.state.seq)  
-// }
-
 function clearTextarea() {
   document.getElementById('seqInput').value = "";  
 }
 
-class App extends Component {
+window.onload = function() {
+  var fileInput = document.getElementById('fileInput');
+  var fileDisplayArea = document.getElementById('fileDisplayArea');
+  fileInput.addEventListener('change', function(e) {
+      var file = fileInput.files[0];
+      var textType = /text.*/;
+      if (file.type.match(textType)) {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+              // fileDisplayArea.innerText = reader.result;
+              console.log(reader.result);
+          }
+          reader.readAsText(file);    
+      } else {
+          fileDisplayArea.innerText = "File not supported!"
+      }
+  });
+}
 
+class App extends Component {
   constructor(props) {
     super(props);
   
@@ -65,6 +75,7 @@ class App extends Component {
     document.getElementById('controlsSeqDisplay').style.display = 'none'; 
   }
 
+
   render() {
     return (
       <div className="App">
@@ -73,20 +84,27 @@ class App extends Component {
             <div className="sequence">
               <div className="seqInput">
                 <h3 style={{textAlign: 'left'}}>Query Sequence</h3><br/><br/>
-                <textarea type="text" id='seqInput' name="seq" onChange={ this.handleChange } value={ this.state.seq } style={{float: 'left', height: '200px', width: '800px'}}></textarea>
+                <textarea type="text" id='seqInput' name="seq" onChange={ this.handleChange } value={ this.state.seq } style={{float: 'left', height: '200px', width: '600px'}}></textarea>
                 <button id="submitButton" style={{float: 'left', marginTop: '20px'}} onClick={this.submitSequence}>Submit Query</button>
+                <button style={{ marginTop: '20px'}} onClick={clearTextarea}>Clear Input</button>
               </div>
               <div className="seqDisplay" id="seqDisplay" style={{display: 'none'}}>
-                <label style={{float: 'left', textAlign: 'left', width: '800px', wordBreak: 'break-all', wordWrap: 'break-word'}}>{ this.state.seq }</label>
+                <label style={{float: 'left', textAlign: 'left', width: '600px', wordBreak: 'break-all', wordWrap: 'break-word'}}>{ this.state.seq }</label>
               </div>
             </div>
             <div className="controls" style={{}}>
-              <h3>Controls</h3><br/><br/>
+              <h3>Search Sequence</h3><br/><br/>
               <div className="controlsSeqInput" id="controlsSeqInput">
-                <button onClick={clearTextarea}>Clear Input</button>
+              <input type="file" id="fileInput"/>
+              <br/><br/><br/>
+                
               </div>
               <div className="controlsSeqDisplay" id="controlsSeqDisplay" style={{display: 'none'}}>
-                <button onClick={this.newSequence}>New Sequence</button>
+              <br/><br/><br/>
+            
+
+        {/* <pre id="fileDisplayArea"></pre> */}
+
               </div>
             </div>
           </div>
