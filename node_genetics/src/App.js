771efc7e8331abdfae2fdb8a97617b0e930453f2
Tracey Wright <traceywright27@gmail.com>
2017-09-12 15:15:01 +1000

    interim commit

--- node_genetics/src/App.js
+++ node_genetics/src/App.js
@@ -5,46 +5,6 @@ function clearTextarea() {
   document.getElementById('seqInput').value = "";  
 }
 
-function tokeniseSequence() {
-  let seq = document.getElementById('fileDisplayArea').textContent;
-  // console.log(seq);
-  let seqArray = seq.match(/.{1,4}/g); // TODO: dynamically create subsequences of the same length as the query
-  // console.log(seqArray);
-  let searchArray = createRotations(seqArray);
-}
-
-function createRotations(seqArr) {
-  let sep;
-  let prev;
-  let rotationArr = [];
-  let regexArray = [];
-  rotationArr[0] = seqArr;
-  regexArray[1] = /.{1,3}/g;
-  regexArray[2] = /.{2,2}/g;
-  regexArray[3] = /.{1}/g;
-  for (let i = 1; i < 4; i++) {
-    rotationArr[i] = [];
-    for (let j = 0; j < seqArr.length - 1; j++) {
-      sep = '';
-      sep = seqArr[j].match(regexArray[i]);
-      if (i < 3) {
-        if (j > 0) {
-          rotationArr[i][j] = prev + sep[0];
-          prev = sep[1];
-        }
-        else { 
-          rotationArr[i][j] = sep[0]; 
-          prev = sep[1];
-        }
-      } else {
-        j > 0 ? rotationArr[i][j] = prev + sep[0] : rotationArr[i][j] = sep[0]; // TODO: Add last element to 4th rotation for full coverage
-        prev = sep[1] + sep[2] + sep[3];
-      }      
-    }
-  }
-  return rotationArr;
-}
-
 window.onload = function() {
   var fileInput = document.getElementById('fileInput');
   var fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -56,9 +16,6 @@ window.onload = function() {
           reader.onload = function(e) {
               fileDisplayArea.innerText = reader.result;
               document.getElementById('createIndexButton').style.display = 'block'; 
-              
-              // console.log(reader.result);
-              // let seqArr = tokeniseSequence(reader.result);
           }
           reader.readAsText(file);    
       } else {
@@ -72,10 +29,14 @@ class App extends Component {
     super(props);
   
     this.state = {
-      seq: []
+      seq: [],
+      seqIndex: []
     }
 
-    this.newSequence = this.newSequence.bind(this);    
+    this.searchIndex = this.searchIndex.bind(this);
+    this.tokeniseSequence = this.tokeniseSequence.bind(this);
+    this.createRotations = this.createRotations.bind(this);
+    // this.newSequence = this.newSequence.bind(this);    
     this.submitSequence = this.submitSequence.bind(this);
     this.handleChange = this.handleChange.bind(this); 
   }
@@ -87,7 +48,7 @@ class App extends Component {
   }
 
   submitSequence() {
-    let seqInput = document.getElementById('seqInput').value;
+    let seqInput = document.getElementById('queryInput').value;
     let charArray = [];
     charArray = seqInput.split('')
     let charOutput = '';
@@ -102,23 +63,70 @@ class App extends Component {
       }
     }
     this.setState({ seq: charOutput })
-    document.getElementById('seqInput').style.display = 'none'; 
+    document.getElementById('queryInput').style.display = 'none'; 
+    document.getElementById('queryDisplay').style.display = 'block'; 
     document.getElementById('submitButton').style.display = 'none';
     document.getElementById('clearButton').style.display = 'none';    
-    document.getElementById('seqDisplay').style.display = 'block'; 
-    document.getElementById('searchSeqInput').style.display = 'none';
-    document.getElementById('searchSeqDisplay').style.display = 'block';  
+    // document.getElementById('searchQueryInput').style.display = 'none';
+    // document.getElementById('searchSeqDisplay').style.display = 'block';  
   }
 
-  newSequence() {
-    this.setState({ seq: '' })
-    document.getElementById('seqInput').style.display = 'block'; 
-    document.getElementById('submitButton').style.display = 'block';
-    document.getElementById('clearButton').style.display = 'block';        
-    document.getElementById('seqDisplay').style.display = 'none'; 
-    document.getElementById('searchSeqInput').style.display = 'block';
-    document.getElementById('searchSeqDisplay').style.display = 'none'; 
+  // newSequence() {
+  //   this.setState({ seq: '' })
+  //   document.getElementById('queryInput').style.display = 'block'; 
+  //   document.getElementById('submitButton').style.display = 'block';
+  //   document.getElementById('clearButton').style.display = 'block';        
+  //   document.getElementById('queryDisplay').style.display = 'none'; 
+  //   document.getElementById('searchQueryInput').style.display = 'block';
+  //   document.getElementById('searchSeqDisplay').style.display = 'none'; 
+  // }
+
+  createRotations(seqArr) {
+    let sep;
+    let prev;
+    let rotationArr = [];
+    let regexArray = [];
+    rotationArr[0] = seqArr;
+    regexArray[1] = /.{1,3}/g;
+    regexArray[2] = /.{2,2}/g;
+    regexArray[3] = /.{1}/g;
+    for (let i = 1; i < 4; i++) {
+      rotationArr[i] = [];
+      for (let j = 0; j < seqArr.length - 1; j++) {
+        sep = '';
+        sep = seqArr[j].match(regexArray[i]);
+        if (i < 3) {
+          if (j > 0) {
+            rotationArr[i][j] = prev + sep[0];
+            prev = sep[1];
+          }
+          else { 
+            rotationArr[i][j] = sep[0]; 
+            prev = sep[1];
+          }
+        } else {
+          j > 0 ? rotationArr[i][j] = prev + sep[0] : rotationArr[i][j] = sep[0]; // TODO: Add last element to 4th rotation for full coverage
+          prev = sep[1] + sep[2] + sep[3];
+        }      
+      }
+    }
+    this.setState({ seqIndex: rotationArr });
   }
+
+  tokeniseSequence() {
+    let seq = document.getElementById('fileDisplayArea').textContent;
+    // console.log(seq);
+    let seqArray = seq.match(/.{1,4}/g); // TODO: dynamically create subsequences of the same length as the query
+    // console.log(seqArray);
+    let searchArray = this.createRotations(seqArray);
+  }
+  
+  searchIndex() {
+    // console.log( this.state.seqIndex );
+    // console.log( this.state.seq )
+
+    
+  } 
 
 
   render() {
@@ -127,22 +135,24 @@ class App extends Component {
         <div style={{display: 'inlineBlock'}}>
           <div className="geneProcesser">
             <div className="query">
-              <div className="seqInput">
+              <div className="queryInput">
                 <h3 style={{textAlign: 'left'}}>Query Sequence</h3><br/><br/>
-                <textarea type="text" id='seqInput' name="seq" onChange={ this.handleChange } value={ this.state.seq } style={{float: 'left', height: '100px', width: '400px'}}></textarea>
+                <textarea type="text" id='queryInput' name="seq" onChange={ this.handleChange } value={ this.state.seq } style={{float: 'left', height: '100px', width: '400px'}}></textarea>
                 <button id="submitButton" style={{float: 'left', marginTop: '20px'}} onClick={this.submitSequence}>Submit Query</button>
                 <button id="clearButton" style={{ marginTop: '20px'}} onClick={clearTextarea}>Clear Input</button>
               </div>
-              <div className="seqDisplay" id="seqDisplay" style={{display: 'none'}}>
-                <label style={{float: 'left', textAlign: 'left', width: '200px', wordBreak: 'break-all', wordWrap: 'break-word'}}>{ this.state.seq }</label>
+              <div className="queryDisplay" id="queryDisplay" style={{display: 'none'}}>
+                <label id="querySeq" style={{float: 'left', textAlign: 'left', width: '200px', wordBreak: 'break-all', wordWrap: 'break-word'}}>{ this.state.seq }</label>
+                <br/><br/><br/>
+                <button onClick={ this.searchIndex } style={{float: 'left', textAlign: 'left'}}>Search</button>
               </div>
             </div>
             <div className="search" style={{}}>
               <h3>Search Sequence</h3><br/><br/>
-              <div className="searchSeqInput" id="searchSeqInput">
+              <div className="searchQueryInput" id="searchQueryInput">
               <input type="file" id="fileInput"/>
               <br/><br/><br/>
-              <button id="createIndexButton" style={{ display: 'none' }} onClick={ tokeniseSequence }>Create Index</button>
+              <button id="createIndexButton" style={{ display: 'none' }} onClick={ this.tokeniseSequence }>Create Index</button>
               <br/><br/><br/>
               <label id="fileDisplayArea" style={{float: 'left', textAlign: 'left', width: '600px', wordBreak: 'break-all', wordWrap: 'break-word'}}></label>
               </div>
