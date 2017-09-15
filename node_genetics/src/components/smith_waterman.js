3743e1908945c8f121f54711fde28d8f29bb67d9
Tracey Wright <traceywright27@gmail.com>
2017-09-15 20:13:22 +1000

    output smith waterman to ui

--- node_genetics/src/components/smith_waterman.js
+++ node_genetics/src/components/smith_waterman.js
@@ -9,6 +9,7 @@ export class Smith_Waterman extends Component {
         this.state = {
             seq1: [],
             seq2: [],
+            optimalAlignment: []
           }
 
         this.handleChange = this.handleChange.bind(this); 
@@ -26,10 +27,6 @@ export class Smith_Waterman extends Component {
     smith_waterm() {
       let seq1 = this.state.seq1;
       let seq2 = this.state.seq2; 
-        console.log(this.state.seq1)
-        console.log(this.state.seq2)
-    //   let seq1 = 'AAAATTTAAGAAAGATGGAGTAAATTTATGTCGGAAAAAGAAATTTGAGT';
-    //   let seq2 = 'AAAATTTAAGAAAGATGGAGTAAATTTAAGATGGAGTAAATTTATGTCGGAAAAAGAAATTTGAGT';
       let gss = function (i) {
         return -i;
       };
@@ -40,7 +37,8 @@ export class Smith_Waterman extends Component {
           return -1;
         }
       };
-      SW.align(seq1, seq2, gss, simfunc)
+      let alignment = SW.align(seq1, seq2, gss, simfunc);
+      this.highlightMatches(alignment, alignment.str1, alignment.str2);
     }
 
     takeSequence() {
@@ -48,6 +46,37 @@ export class Smith_Waterman extends Component {
         console.log(s) 
         this.setState({ seq1: s })
       }
+
+    highlightMatches(alignment, seq1, seq2) {
+        let sq1 = seq1.split('');
+        let sq2 = seq2.split('');
+        let target = document.getElementById('resultDisplayArea');
+        let line1;
+        let line2; 
+        for (let i = 0; i < alignment.walk.length; i++) {
+            if (i % 100 === 0){
+                line1 = document.createElement('div');
+                line2 = document.createElement('div');
+                target.appendChild(line1);
+                target.appendChild(line2);
+            }
+            let elem1 = document.createElement('span');
+            let elem2 = document.createElement('span');
+            let text1 = document.createTextNode(sq1[i]);
+            let text2 = document.createTextNode(sq2[i]);
+            elem1.appendChild(text1);
+            elem2.appendChild(text2);
+            line1.appendChild(elem1);
+            line2.appendChild(elem2);
+
+            if (seq1[i] === seq2[i]) {
+                elem1.style.color = 'red';
+                elem1.style.fontWeight = 'bold';
+                elem2.style.color = 'red';
+                elem2.style.fontWeight = 'bold';
+            }
+        }
+    }   
 
       render() {
         return (
@@ -57,6 +86,9 @@ export class Smith_Waterman extends Component {
                 <button id="createIndexButton" onClick={ this.takeSequence }>Create Index</button>
                 <br/><br/><br/>
                 <textarea placeholder="Enter a Query Sequence" type="text" id='' name="seq2" onChange={ this.handleChange } value={ this.state.seq2 } style={{ height: '100px', width: '400px'}}></textarea>
+                <br/><br/><br/>
+                <div id="resultDisplayArea"/>
+
             </div>
         );
     }
