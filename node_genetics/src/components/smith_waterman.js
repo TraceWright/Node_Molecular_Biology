3dd645917e0223c329a2f22f4930b3818eb8642b
Tracey Wright <traceywright27@gmail.com>
2017-09-15 13:08:05 +1000

    interim commit

--- node_genetics/src/components/smith_waterman.js
+++ node_genetics/src/components/smith_waterman.js
@@ -3,7 +3,10 @@ import '../App.css';
 import * as SW from 'igenius-smith-waterman';
 
 function smith_waterm() {
-  let seq1 = 'AAAATTTAAGAAAGATGGAGTAAATTTATGTCGGAAAAAGAAATTTGAGT';
+  let seq1 = document.getElementById('fileDisplayArea').textContent;
+    
+    console.log(this.state.querySeq)
+//   let seq1 = 'AAAATTTAAGAAAGATGGAGTAAATTTATGTCGGAAAAAGAAATTTGAGT';
   let seq2 = 'AAAATTTAAGAAAGATGGAGTAAATTTAAGATGGAGTAAATTTATGTCGGAAAAAGAAATTTGAGT';
   let gss = function (i) {
     return -i;
@@ -21,11 +24,37 @@ function smith_waterm() {
 export class Smith_Waterman extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            searchSeq: [],
+            querySeq: [],
+          }
+
+        this.handleChange = this.handleChange.bind(this); 
+        this.takeSequence = this.takeSequence.bind(this); 
+        
     }
+
+    handleChange({ target }) {
+        this.setState({
+            [target.name]: target.value
+          });
+      }
+
+    takeSequence() {
+        let s = document.getElementById('fileDisplayArea').textContent;
+        console.log(s);
+        this.setState({ searchSeq: s })
+      }
+
       render() {
         return (
             <div>
                 <button onClick={ smith_waterm }>Smith-Waterman</button>
+                <br/><br/><br/>
+                <button id="createIndexButton" onClick={ this.takeSequence }>Create Index</button>
+                <br/><br/><br/>
+                <textarea placeholder="Enter a Query Sequence" type="text" id='' name="querySeq" onChange={ this.handleChange } value={ this.state.seq } style={{ height: '100px', width: '400px'}}></textarea>
             </div>
         );
     }
