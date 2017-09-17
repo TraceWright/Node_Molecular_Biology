2502ce5fd70a8ee9c6a5da705ffb7422e4535e6f
Tracey Wright <traceywright27@gmail.com>
2017-09-17 12:27:05 +1000

    added multiple file upload capability

--- node_genetics/src/App.js
+++ node_genetics/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-// import * as fs from 'browserify-fs';
+import * as fs from 'browserify-fs';
 
 import { simple_starter } from './components/simple_starter';
 import { Smith_Waterman } from './components/smith_waterman';
 
+function uploadFile(fileName, fileText) {
+  // console.log(fileName);
+  // console.log(fileText);
+  fs.mkdir('/home', function() {
+    fs.writeFile(`/home/${fileName}`, fileText, function () {
+      fs.readFile(`/home/${fileName}`, 'utf-8', function(err, data) {
+        //console.log(data);
+      });
+    });
+  });
+}
+
 window.onload = function() {
   let fileInput = document.getElementById('fileInput');
   let fileDisplayArea = document.getElementById('fileDisplayArea');
+  let dirContents  = document.getElementById('dir-content');
+  console.log('dc: ' + dirContents)
+  let fileList;
   fileInput.addEventListener('change', function(e) {
       var file = fileInput.files[0];
       var textType = /text.*/;
@@ -16,24 +31,34 @@ window.onload = function() {
           var reader = new FileReader();
           reader.onload = function(e) {
             fileDisplayArea.innerText = reader.result;
+            uploadFile(fileInput.files[0].name, fileDisplayArea.innerText);
           }
           reader.readAsText(file);    
       } else {
           fileDisplayArea.innerText = "File not supported!"
       }
+
+      fileList = dirContents.innerText + fileInput.files[0].name + '\r\n';
+      dirContents.innerText = fileList;
+
   });
 }
 
-// fs.mkdir('/home', function() {
-//   fs.writeFile('/home/hello-world.txt', 'Hello world!\n', function() {
-//       fs.readFile('/home/hello-world.txt', 'utf-8', function(err, data) {
-//           console.log(data);
-//       });
-//   });
-// });
-// fs.readdir("/home/",function(e,f) {console.log(f);})
+function clearBrowserFiles() {
+  let dirContents  = document.getElementById('dir-contents');
+  dirContents.innerText = 'Directory Contents: ';
+    fs.readdir('/home', function(e,f) {
+      console.log(f);
+    
+      f.forEach(function(element) {
+        fs.unlink('/home/' + element)
+        console.log(element)
+      });
+  
+    })
+  }
 
-function uploadFiles() {
+function uploadFilesPage() {
   document.getElementById('file-uploader').style.display = 'grid'; 
   document.getElementById('search-components').style.display = 'none';
   document.getElementById('uploaded-sequence').style.display = 'grid';  
@@ -60,17 +85,26 @@ class App extends Component {
          <div style={{display: 'inlineBlock'}}>
           <div>
             <div className="files">
-              <button className='bttn' id="upload-files-btn" style={{ float: 'left', marginTop: '20px', marginLeft: '20px' }} onClick={ uploadFiles }>Upload File/s</button>
+              <button className='bttn' id="upload-files-btn" style={{ float: 'left', marginTop: '20px', marginLeft: '20px' }} onClick={ uploadFilesPage }>Upload File/s</button>
               <button className='bttn' id="back-btn" style={{ float: 'left', marginTop: '20px', marginLeft: '20px', display: 'none' }} onClick={ back }>Back</button>
             </div>
 
             <div style={{ display: 'none'}} className="file-uploader" id="file-uploader">
+
+              <div className="dir-contents">
+                <div id="dir-content">Directory Contents: </div>
+              </div>
+
+            <div className="clear-dir">
+                <button onClick={ clearBrowserFiles }>Clear Directory</button>
+              </div>
+
               <div className="input-files">
                 <input type="file" id="fileInput"/>
               </div> 
 
               <div style={{ display: 'none', float: 'left' }} className="uploaded-sequence" id="uploaded-sequence">    
-                <label style={{ textAlign: 'left' }}>Uploaded Sequence:</label>
+                <label style={{ textAlign: 'left' }}>File Contents:</label>
                 <br/><br/>
                 <label id="fileDisplayArea" style={{float: 'left', textAlign: 'left', width: '600px', wordBreak: 'break-all', wordWrap: 'break-word'}}></label>
               </div>
